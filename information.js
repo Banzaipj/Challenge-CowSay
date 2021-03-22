c06ddceb7e308c5dd45a87ec8972006a68a3e52c
Pascal Joly <jolypascal20@gmail.com>
2021-03-22 12:32:29 +0100

    CowSay

---
+++ information.js
@@ -0,0 +1,12 @@
+let name = "Pascal JOLY";
+
+let campusname = "Wild Code School Reims"; 
+
+const myInfos = () => {
+    console.log (`Hello I'm ${name} from ${campusname}`);
+}
+
+module.exports = myInfos;
+
+
+
